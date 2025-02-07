676a1fe5f4fa31b645919e5982910894f90139f8
whatuserever <83753332+whatuserever@users.noreply.github.com>
2025-02-07 10:30:28 +0200

    Add next-auth

---
+++ src/app/lib/auth.config.ts
@@ -0,0 +1,35 @@
+import type { NextAuthConfig } from "next-auth";
+import CredentialProvider from 'next-auth/providers/credentials';
+
+export const authConfig = {
+  providers: [
+    CredentialProvider({
+      credentials: {
+        email: {
+          type: 'email'
+        },
+        password: {
+          type: 'password'
+        }
+      },
+      async authorize(credentials, req) {
+        const user = {
+          id: '1',
+          name: 'John',
+          email: credentials?.email as string
+        };
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user;
+        } else {
+          // If you return null then an error will be displayed advising the user to check their details.
+          return null;
+
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        }
+      }
+    })
+  ],
+} satisfies NextAuthConfig
+
+export default authConfig;
